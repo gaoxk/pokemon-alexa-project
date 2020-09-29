2a9de2b63cc973c2551046fc57e638cc035836ba
gaoxk <gaoxuekristy@gmail.com>
2020-09-28 22:01:16 -0700

    modurlarized pokemon utils and api helpers

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const Alexa = require('ask-sdk-core');
 const express = require('express');
-const axios = require('axios');
+const pokemonUtil = require('./pokemon')
 
 const { ExpressAdapter } = require('ask-sdk-express-adapter');
-
-const POKEMON_URL_BASE = "https://pokeapi.co/api/v2/pokemon";
-
-const UNKNOWN_POKEMON = "Hmmmm I don't think I'm familiar with that pokemon 👀 could you try again?"
-const UNKNOWN_TRAIT_STATMENT = " is something I am not sure about 🤔 ";
 
 const LAUNCH_INTENT_MSG = "Hello! Welcome to your pokedex 🐼"
 const HELP_INTENT_MSG = "Ask me anything about pokemon! For example, 'what is pikachu's height?' Would you like to continue? (yes/no)";
@@ -17,31 +12,6 @@ const YES_INTENT_MSG = "Yay! ask me more about pokemon!!👺" ;
 const app = express();
 const port = 3000;
 const skillBuilder = Alexa.SkillBuilders.custom();
-
-//common util
-const pokemonNameSanitizer = name => {
-    return name.replace("'s", "")
-            .normalize("NFD")
-            .replace(/[\u0300-\u036f]/g, "")
-            .toLowerCase();
-}
-
-//Pokemon API utils
-//TODO: throw error for invalid pokemon name request
-const getPokemonInfo = async (name) => {
-    const pokemonInfoUrl = POKEMON_URL_BASE + "/" + name;
-    return await axios.get(pokemonInfoUrl);
-}
-
-const getPokemonTrait = async (name, trait) => {
-    const data = await getPokemonInfo(name);
-    return getPokemonTraitFromData(data, trait);
-}
-const getPokemonTraitFromData = (data, trait) => {
-    //TODO: expand implementation to return information nested in json return (eg, types)
-    return !data.data[trait] ?
-     trait + UNKNOWN_TRAIT_STATMENT : trait + " is " + data.data[trait];
-}
 
 //Request Handlers  
 const LaunchRequestHandler = {
@@ -102,10 +72,10 @@ const PokemonInfoHandler = {
     },
     async handle(handlerInput) {
         const pokemonName =
-            pokemonNameSanitizer(handlerInput.requestEnvelope.request.intent.slots.pokemon.value);
-        const data = await getPokemonInfo(pokemonName);
-        const heightStatement = getPokemonTraitFromData(data, "height");
-        const weightStatement = getPokemonTraitFromData(data, "weight");
+            pokemonUtil.pokemonNameSanitizer(handlerInput.requestEnvelope.request.intent.slots.pokemon.value);
+        const data = await pokemonUtil.getPokemonData(pokemonName);
+        const heightStatement = pokemonUtil.getPokemonTraitFromData(data, "height");
+        const weightStatement = pokemonUtil.getPokemonTraitFromData(data, "weight");
     
         const speechText = pokemonName + " is a pokemon, whos " 
              + heightStatement + " and " 
@@ -126,11 +96,11 @@ const PokemonTraitHandler = {
     },
     async handle(handlerInput) {
         const pokemonName =
-            pokemonNameSanitizer(handlerInput.requestEnvelope.request.intent.slots.pokemon.value);
+            pokemonUtil.pokemonNameSanitizer(handlerInput.requestEnvelope.request.intent.slots.pokemon.value);
         const pokemonTrait =
             handlerInput.requestEnvelope.request.intent.slots.trait.value;
  
-        const traitStatement = await getPokemonTrait(pokemonName, pokemonTrait);
+        const traitStatement = await pokemonUtil.getPokemonTrait(pokemonName, pokemonTrait);
         const speechText = pokemonName + "'s " 
              + traitStatement + ". "
              + "What else would you like to know?";
---
+++ pokemon.js
@@ -0,0 +1,38 @@
+//Pokemon utils
+const axios = require('axios');
+
+const POKEMON_URL_BASE = "https://pokeapi.co/api/v2/pokemon";
+//TODO: Introduce error handling that utilies this
+const UNKNOWN_POKEMON = "Hmmmm I don't think I'm familiar with that pokemon 👀 could you try again?"
+const UNKNOWN_TRAIT_STATMENT = " is something I am not sure about 🤔 ";
+
+const pokemonNameSanitizer = name => {
+    return name.replace("'s", "")
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .toLowerCase();
+}
+
+//Pokemon API utils
+//TODO: throw error for invalid pokemon name request
+const getPokemonData = async (name) => {
+    const pokemonInfoUrl = POKEMON_URL_BASE + "/" + name;
+    return await axios.get(pokemonInfoUrl);
+}
+
+const getPokemonTrait = async (name, trait) => {
+    const data = await getPokemonData(name);
+    return getPokemonTraitFromData(data, trait);
+}
+const getPokemonTraitFromData = (data, trait) => {
+    //TODO: expand implementation to return information nested in json return (eg, types)
+    return !data.data[trait] ?
+     trait + UNKNOWN_TRAIT_STATMENT : trait + " is " + data.data[trait];
+}
+
+module.exports = {
+    pokemonNameSanitizer,
+    getPokemonData,
+    getPokemonTrait,
+    getPokemonTraitFromData
+}
